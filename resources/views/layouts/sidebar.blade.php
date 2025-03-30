1087ff7de1e08a54386f8fb920d965801163857a
wayanjanuarta25 <datacrackers.bali@gmail.com>
2025-03-30 20:14:12 +0700

    Inventech

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,7 +1,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="{{ url('/') }}" class="brand-link">
-        <span class="brand-text font-weight-light">AdminLTE</span>
+        <span class="brand-text font-weight-light">INVENTECH</span>
     </a>
 
     <!-- Sidebar -->
